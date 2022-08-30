34eebf66aeb610de11ce7a4f97cdd27ec0d9d82f
Kyle <53420521+DigitalMoose28@users.noreply.github.com>
2022-08-30 12:20:08 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ ansible-playbook playbooks/SO_hard_install.yaml
 ansible-playbook playbooks/SO_Manager_Search_Deploy.yaml
 ansible-playbook playbooks/SO_MGR_Config.yaml
 ansible-playbook playbooks/SO_MGR_setup.yaml
+ansible-playbook playbooks/SO_MGR_Cleanup.yaml
 ansible-playbook playbooks/SO_Search_config.yaml
 ansible-playbook playbooks/SO_Search_setup.yaml
 ansible-playbook playbooks/SO_Sensor_config.yaml
